68dc81d0358f5cbddd34aa527de776590a7f78f4
Agner Souza Bezerra <agner.bezerra@toledobrasil.com>
2025-05-30 15:37:54 -0300

    Add the getCurrencyConversion function in index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import { getCurrencyConversion } from '../controllers/currency-converter.controller';
 
 const router = Router();
 
-
+router.get('/currency-converter', getCurrencyConversion);
 
 export default router;
