4fc1aea1adc61bf2407f595278805f2b096d4bdb
Agner Souza Bezerra <agner.bezerra@toledobrasil.com>
2025-05-30 15:29:26 -0300

    Add the route call in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import router from './routes';
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+// Routes
+app.use('/api', router);
 
 // Health check
 app.get('/health', (req, res) => {
