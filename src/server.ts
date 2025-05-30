d64917e7d59d613daa721e082e55c38fcb2f1494
Agner Souza Bezerra <agner.bezerra@toledobrasil.com>
2025-05-30 16:36:38 -0300

    adjust the enviroment vairables

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import app from './app';
+import app from './app'
 import dotenv from 'dotenv';
 
 dotenv.config();
+const url = process.env.APP_URL ?? '';
+const port = process.env.PORT ?? '';
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`[server]: Server is running at ${url}:${port}`);
 });
