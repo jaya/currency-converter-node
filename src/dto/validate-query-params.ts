d59d423ca76a15fd0a4bb6387f6390a31a827bdd
Agner Souza Bezerra <agner.bezerra@toledobrasil.com>
2025-06-04 19:10:41 -0300

    Create the ValidateQueryParams class in order to validate de DTO data.

---
+++ src/dto/validate-query-params.ts
@@ -0,0 +1,43 @@
+import { ConvertCurrencyDTO, ValidatedConvertCurrency } from "../interfaces/convert-currency-dto.interface";
+import { CurrencyType } from "../interfaces/currency-type.interface";
+
+export class ValidateQueryParams {
+
+  private currencyType = CurrencyType;
+
+  validateCurrencyParams(query: ConvertCurrencyDTO | null): ValidatedConvertCurrency {
+    const validCurrencies = [this.currencyType.BRL, this.currencyType.USD, this.currencyType.EUR, this.currencyType.JPY];
+
+    if (typeof query !== 'object' || query === null) {
+      throw new Error('Invalid query parameters: expected an object');
+    }
+    if (!query.fromCurrency) {
+      throw new Error('Missing fromCurrency query parameter');
+    }
+    if (!validCurrencies.includes(query.fromCurrency)) {
+      throw new Error(`The fromCurrency property must be one of: ${validCurrencies.join(', ')}`);
+    }
+
+    if (!query.toCurrency) {
+      throw new Error('Missing toCurrency query parameter');
+    }
+    const fromValueNumber = parseFloat(String(query.fromValue));
+    if (isNaN(fromValueNumber)) {
+      throw new Error('The query parameter toCurrency must be a number');
+    }
+    if (fromValueNumber <= 0) {
+      throw new Error('The query parameter toCurrency must be greater than zero');
+    }
+    if (!query.toCurrency) {
+      throw new Error('Missing toCurrency query parameter');
+    }
+    if (!validCurrencies.includes(query.toCurrency)) {
+      throw new Error(`The toCurrency property must be one of: ${validCurrencies.join(', ')}`);
+    }
+    return {
+      fromCurrency: query.fromCurrency,
+      fromValue: fromValueNumber,
+      toCurrency: query.toCurrency
+    };
+  }
+}
