a0a2d618f12d1c60825904f422c0dc878ce31317
Agner Souza Bezerra <agner.bezerra@toledobrasil.com>
2025-06-04 19:10:09 -0300

    Adjust the controller in order to validate the DTO data.

--- src/controllers/currency-converter.controller.ts
+++ src/controllers/currency-converter.controller.ts
@@ -1,5 +1,26 @@
 import { Request, Response } from 'express';
+import { CurrencyConverterService } from '../services/currency-converter.service';
+import { ValidateQueryParams } from '../dto/validate-query-params';
+import { ConvertCurrencyDTO } from '../interfaces/convert-currency-dto.interface';
+import { CurrencyType } from '../interfaces/currency-type.interface';
 
 export const getCurrencyConversion = (req: Request, res: Response) => {
-  res.json({ message: 'Current Conversion successful' });
+  try {
+    const validator = new ValidateQueryParams();
+    const queryParams: ConvertCurrencyDTO = {
+      fromCurrency: req.query.fromCurrency as CurrencyType,
+      toCurrency: req.query.toCurrency as CurrencyType,
+      fromValue: req.query.fromValue as string
+    };
+
+    const validatedParams = validator.validateCurrencyParams(queryParams);
+
+    const currencyConverterService = new CurrencyConverterService();
+    // currencyConverterService.convertCurrencyAndGetTaxes(fromCurrency, fromValue,toCurrency)
+    res.json({ message: 'Current Conversion successful' });
+  } catch (error: any) {
+    console.error("ERROR_VALIDATE_DTO " + error.message);
+    res.status(400).json({ Error: error.message })
+  }
+
 };
