21bf311475502359c5cf0d0144f61511faed74a9
Agner Souza Bezerra <agner.bezerra@toledobrasil.com>
2025-06-06 17:10:38 -0300

    Adjust the CurrencyConverterService class.

--- src/services/currency-converter.service.ts
+++ src/services/currency-converter.service.ts
@@ -1,31 +1,38 @@
 import { ConversionResultInterface } from "../interfaces/conversion-result.interface";
+import { ValidatedConvertCurrency } from "../interfaces/convert-currency-dto.interface";
 import { ParamsApiCallInteface } from "../interfaces/params-api-call.interface";
 import { CallApiService } from "./call-api.service";
 
 export class CurrencyConverterService {
   constructor() { }
-  async convertCurrencyAndGetTaxes(fromCurrency: string, fromValue: number, toCurrency: string) {
+  async convertCurrencyAndGetTaxes(validatedParams: ValidatedConvertCurrency) {
+    const { fromCurrency, toCurrency, fromValue, } = validatedParams
+
     const params: ParamsApiCallInteface = {
       apikey: process.env.API_KEY ?? '',
-      base_currency: fromCurrency,
-      currencies: toCurrency
+      base_currency: fromCurrency ?? '',
+      currencies: toCurrency ?? ''
     }
+
     const apiUrl = process.env.API_URL ?? '';
     const callApiService = new CallApiService();
     const apiResult = await callApiService.callApiWithGetAndParams(apiUrl, params);
-    const rate = apiResult.data.data.USD.value;
-    const timestamp = apiResult.data.meta.last_updated_at;
+    console.log(apiResult);
+
+
+    const rate = apiResult?.data.data.USD.value;
+    const timestamp = apiResult?.data?.meta.last_updated_at;
 
     const toValue = fromValue * rate;
 
     const conversionResult: ConversionResultInterface = {
-      fromCurrency,
-      toCurrency,
-      fromValue,
+      fromCurrency: fromCurrency ?? '',
+      toCurrency: toCurrency ?? '',
+      fromValue: fromValue,
       toValue: parseFloat(toValue.toFixed(4)),
       rate,
       timestamp
     };
-    console.log(conversionResult);
+    return conversionResult;
   }
 }
