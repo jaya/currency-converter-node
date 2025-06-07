ccb85ce9ee54dfbc0245f6a01e26ce004a47a48e
Agner Bezerra <agnerbzrao@hotmail.com>
2025-06-07 16:14:40 -0300

    Add the dependency injection in CurrencyConverterService class.

--- src/services/currency-converter.service.ts
+++ src/services/currency-converter.service.ts
@@ -1,24 +1,24 @@
-import { ConversionResultInterface } from "../interfaces/conversion-result.interface";
-import { ValidatedConvertCurrency } from "../interfaces/convert-currency-dto.interface";
-import { ParamsApiCallInteface } from "../interfaces/params-api-call.interface";
-import { CallApiService } from "./call-api.service";
+import { ConversionResultInterface } from '../interfaces/conversion-result.interface';
+import { ValidatedConvertCurrency } from '../interfaces/convert-currency-dto.interface';
+import { ParamsApiCallInteface } from '../interfaces/params-api-call.interface';
+import { CallApiService } from './call-api.service';
 
 export class CurrencyConverterService {
-  constructor() { }
+  constructor(private callApiService: CallApiService) {}
   async convertCurrencyAndGetTaxes(validatedParams: ValidatedConvertCurrency) {
-    const { fromCurrency, toCurrency, fromValue, } = validatedParams
-
+    const { fromCurrency, toCurrency, fromValue } = validatedParams;
     const params: ParamsApiCallInteface = {
       apikey: process.env.API_KEY ?? '',
       base_currency: fromCurrency ?? '',
-      currencies: toCurrency ?? ''
-    }
+      currencies: toCurrency ?? '',
+    };
 
     const apiUrl = process.env.API_URL ?? '';
-    const callApiService = new CallApiService();
-    const apiResult = await callApiService.callApiWithGetAndParams(apiUrl, params);
-    console.log(apiResult);
 
+    const apiResult = await this.callApiService.callApiWithGetAndParams(
+      apiUrl,
+      params
+    );
 
     const rate = apiResult?.data.data.USD.value;
     const timestamp = apiResult?.data?.meta.last_updated_at;
@@ -31,7 +31,7 @@ export class CurrencyConverterService {
       fromValue: fromValue,
       toValue: parseFloat(toValue.toFixed(4)),
       rate,
-      timestamp
+      timestamp,
     };
     return conversionResult;
   }
