f4c27a1b73693a93a9a0929dc1db556dc9c6811e
Agner Souza Bezerra <agner.bezerra@toledobrasil.com>
2025-06-09 22:01:18 -0300

    Add the middleware error handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import router from './routes';
+import { errorHandler } from './middlewares/error.middleware';
 
 const app = express();
 
@@ -9,13 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use(errorHandler);
 // Routes
 app.use('/', router);
 
 // Health check
 app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'OK' });
+  res.status(200).json({ status: 'OK' });
 });
 
 export default app;
